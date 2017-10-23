af906ffd670988b928c0a45611503663232a62ba
anthonywebster <anthonyanroab@gmail.com>
2017-10-23 13:06:57 -0600

    clean models

--- app/Client.php
+++ app/Client.php
@@ -7,7 +7,10 @@ class Client extends Base
     protected $table = 'clients';
 
     protected $guarded = [];
-
+    
+    /**
+     * @return mixed
+     */
     public function jobs() {
         return $this->hasMany(Job::class, 'client_id', 'id');
     }
