61b59dcd536031701d1c483d018c0000f08904c5
anthony webster <anthonyanroab@gmail.com>
2018-10-27 21:25:30 -0600

    refactoring 2018

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -27,6 +27,7 @@ class ClientsController extends Controller
     public function create()
     {
         $breadcrumbs = $this->getBreadCrumbs($this->bc, ['Activas' => '-']);
+
         return view('clients.create');
     }
 
@@ -35,40 +36,43 @@ class ClientsController extends Controller
         $input = array_except($request->all(), ['_token']);
         $input['slug'] = $this->slugify($input['name']);
         $created = $this->model->create($input);
+
         if ($created) {
             return redirect('clients')->with('success', 'Tu cliente fue agregada a la lista de activas.');
-        } else {
-            return redirect()->withInput($input)->with('error', 'It was not possible to create the new client.');
         }
+        return redirect()->withInput($input)->with('error', 'It was not possible to create the new client.');
     }
 
     public function edit($id)
     {
         $client = $this->model->find( $id);
+
         return view('clients.edit', compact('client'));
     }
 
     public function update($id, ClientsForm $request)
     {
         $client = $this->model->find($id);
+
         $input = array_except($request->all(), ['_token']);
         $input['slug'] = $this->slugify($input['name']);
         $updated = $client->update($input);
+
         if ($updated) {
             return redirect('clients')->with('success', 'Tu cliente fue actualizada.');
-        } else {
-            return redirect()->withInput($input)->with('error', 'It was not possible to update the client.');
         }
+        return redirect()->withInput($input)->with('error', 'It was not possible to update the client.');
     }
 
     public function delete($id) {
         $client = $this->model->find($id);
+
         if ($client) {
             $client->jobs()->delete();
             $client->delete();
             return redirect('clients')->with('success', 'El cliente fue eliminado.');
-        } else {
-            return redirect('clients')->with('error', 'Ocurrio un error tratando de eliminar esta cliente.');
         }
+
+        return redirect('clients')->with('error', 'Ocurrio un error tratando de eliminar esta cliente.');
     }
 }
